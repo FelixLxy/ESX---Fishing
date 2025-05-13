0521449e83da098fe5cc6d3b1891345cb6bcd6cb
FelixLxy <felixlackner27@gmail.com>
2025-05-13 12:48:40 +0200

    ESX - Fishing (Update)

--- fishing/client/main.lua
+++ fishing/client/main.lua
@@ -6,9 +6,9 @@ local bait = 'none'
 
 debugPrint = function(msg, type)
 	if Config.Debug then 
-		if 'info' then 
+		if type == 'info' then 
 			print('['..GetCurrentResourceName()..'] [^5INFO^7] '..tostring(msg))
-		elseif 'error' then 
+		elseif type == 'error' then 
 			print('['..GetCurrentResourceName()..'] [^1ERROR^7] '..tostring(msg))
 		else 
 			print('['..GetCurrentResourceName()..'] [DEBUG] '..tostring(msg))
@@ -39,11 +39,10 @@ CreateThread(function()
 				fishing = false
 				TriggerEvent('fishing:break')
 				lib.notify({
-					description = 'Du hast deine Angelrute eingeholt', 
+					description = 'You reeled in your fishing rod', 
 					type = 'inform',
-					
-Duration = true,
-    				position = 'top',
+					showDuration = true,
+					position = 'top',
 				})
 			end
 
@@ -51,10 +50,10 @@ Duration = true,
 				fishing = false
 				TriggerEvent('fishing:break')
 				lib.notify({
-					description = 'Du hast deine Angelrute eingeholt', 
+					description = 'You reeled in your fishing rod', 
 					type = 'inform',
 					showDuration = true,
-    				position = 'top',
+					position = 'top',
 				})
 			end
 		else 
@@ -66,6 +65,8 @@ end)
 startFishingSkill = function()
 	if not isBusy then 
 		debugPrint('Starting SkillCheck', 'info')
+		isBusy = true
+		local success
 		if bait == 'shark' then 
 			success = lib.skillCheck({'easy','hard','medium','medium','hard'})
 		else
@@ -75,18 +76,17 @@ startFishingSkill = function()
 		if success then
 			debugPrint('Skillcheck is successful and script is not busy.', 'info')
 			TriggerServerEvent('fishing:catch', bait)
-			isBusy = false
 		else
 			lib.cancelSkillCheck()
 			debugPrint('Skillcheck is failed or script is busy.', 'error')
 			lib.notify({
-				description = 'Der Fisch konnte sich losreißen', 
+				description = 'The fish got away', 
 				type = 'error',
 				showDuration = true,
 				position = 'top',
 			})
-			isBusy = false
 		end 
+		isBusy = false
 	end
 end
 
@@ -98,6 +98,7 @@ RegisterNetEvent('fishing:break', function()
 	end
 
 	StopFishingAnimation()
+	TriggerServerEvent('fishing:stop')
 end)
 
 RegisterNetEvent('fishing:setbait', function(bool)
@@ -111,79 +112,95 @@ RegisterNetEvent('fishing:fishstart', function()
 	debugPrint('Started fishing at '..playerPos, 'info')
 
 	if IsPedInAnyVehicle(playerPed) then
-		lib.notify({description = 'Das fischen im Fahrzeug ist nicht möglich', position = 'top', type = 'error'})
+		lib.notify({
+			description = 'Fishing from a vehicle is not allowed', 
+			position = 'top', 
+			type = 'error'
+		})
 	else
 		if playerPos.y >= 8200 or playerPos.y <= -4000 or playerPos.x <= -3500 or playerPos.x >= 4200 then
 
 			if fishing then 
-				lib.notify({description = 'Du hast deine Angel bereits ausgeworfen', position = 'top', type = 'error'})
+				lib.notify({
+					description = 'You have already cast your fishing rod', 
+					position = 'top', 
+					type = 'error'
+				})
 				return
 			end
 
-			lib.notify({description = 'Du hast deine Angel ausgeworfen', position = 'top', type = 'success'})
+			lib.notify({
+				description = 'You cast your fishing rod', 
+				position = 'top', 
+				type = 'success'
+			})
 
 			StartFishingAnimation()
 			
 			fishing = true
+			TriggerServerEvent('fishing:start')
 			debugPrint('set fishing to: '..tostring(fishing), 'info')
 		else
-			lib.notify({description = 'Du bist zu nah an der Küste', position = 'top', type = 'error'})
+			lib.notify({
+				description = 'You are too close to the shore', 
+				position = 'top', 
+				type = 'error'
+			})
 		end
 	end
-end, false)
+end)
 
 local fishingAnimation = {
-    dict = "amb@world_human_stand_fishing@idle_a",
-    anim = "idle_a",
-    scenario = "Fishing 1",
-    propModel = 'prop_fishing_rod_01',
-    propBone = 60309,
-    propPlacement = {x = 0.0, y = 0.0, z = 0.0, xRot = 0.0, yRot = 0.0, zRot = 0.0},
-    emoteLoop = true,
-    emoteMoving = false,
-    propEntity = nil
+	dict = "amb@world_human_stand_fishing@idle_a",
+	anim = "idle_a",
+	scenario = "Fishing 1",
+	propModel = 'prop_fishing_rod_01',
+	propBone = 60309,
+	propPlacement = {x = 0.0, y = 0.0, z = 0.0, xRot = 0.0, yRot = 0.0, zRot = 0.0},
+	emoteLoop = true,
+	emoteMoving = false,
+	propEntity = nil
 }
 
 function StartFishingAnimation()
-    RequestAnimDict(fishingAnimation.dict)
-    while not HasAnimDictLoaded(fishingAnimation.dict) do
-        Wait(0)
-    end
+	RequestAnimDict(fishingAnimation.dict)
+	while not HasAnimDictLoaded(fishingAnimation.dict) do
+		Wait(0)
+	end
 
 	debugPrint('starting animation', 'info')
-    TaskPlayAnim(PlayerPedId(), fishingAnimation.dict, fishingAnimation.anim, 8.0, -8.0, -1, fishingAnimation.emoteLoop, 0, false, false, false)
-    
-    local playerPed = PlayerPedId()
-    local boneIndex = GetPedBoneIndex(playerPed, fishingAnimation.propBone)
-    
-    if boneIndex ~= -1 then
-        local boneCoords = GetWorldPositionOfEntityBone(playerPed, boneIndex)
-        local propCoords = vector3(boneCoords.x, boneCoords.y, boneCoords.z)
-        
-        fishingAnimation.propEntity = CreateObject(GetHashKey(fishingAnimation.propModel), propCoords.x, propCoords.y, propCoords.z, true, true, true)
-        
-        if DoesEntityExist(fishingAnimation.propEntity) then
-            AttachEntityToEntity(fishingAnimation.propEntity, playerPed, boneIndex, fishingAnimation.propPlacement.x, fishingAnimation.propPlacement.y, fishingAnimation.propPlacement.z, fishingAnimation.propPlacement.xRot, fishingAnimation.propPlacement.yRot, fishingAnimation.propPlacement.zRot, true, true, false, true, 1, true)
-        end
-    end
+	TaskPlayAnim(PlayerPedId(), fishingAnimation.dict, fishingAnimation.anim, 8.0, -8.0, -1, fishingAnimation.emoteLoop, 0, false, false, false)
+	
+	local playerPed = PlayerPedId()
+	local boneIndex = GetPedBoneIndex(playerPed, fishingAnimation.propBone)
+	
+	if boneIndex ~= -1 then
+		local boneCoords = GetWorldPositionOfEntityBone(playerPed, boneIndex)
+		local propCoords = vector3(boneCoords.x, boneCoords.y, boneCoords.z)
+		
+		fishingAnimation.propEntity = CreateObject(GetHashKey(fishingAnimation.propModel), propCoords.x, propCoords.y, propCoords.z, true, true, true)
+		
+		if DoesEntityExist(fishingAnimation.propEntity) then
+			AttachEntityToEntity(fishingAnimation.propEntity, playerPed, boneIndex, fishingAnimation.propPlacement.x, fishingAnimation.propPlacement.y, fishingAnimation.propPlacement.z, fishingAnimation.propPlacement.xRot, fishingAnimation.propPlacement.yRot, fishingAnimation.propPlacement.zRot, true, true, false, true, 1, true)
+		end
+	end
 end
 
 function StopFishingAnimation()
-    if DoesEntityExist(fishingAnimation.propEntity) then
-        DeleteEntity(fishingAnimation.propEntity)
-        fishingAnimation.propEntity = nil
-    end
+	if DoesEntityExist(fishingAnimation.propEntity) then
+		DeleteEntity(fishingAnimation.propEntity)
+		fishingAnimation.propEntity = nil
+	end
 
-    ClearPedTasks(PlayerPedId())
-    RemoveAnimDict(fishingAnimation.dict)
+	ClearPedTasks(PlayerPedId())
+	RemoveAnimDict(fishingAnimation.dict)
 end
-
 
 RegisterNetEvent('fishing:spawnPed', function()
 	RequestModel(GetHashKey("A_C_SharkTiger"))
-		while (not HasModelLoaded(GetHashKey( "A_C_SharkTiger"))) do
-			Wait(20)
-		end
+	while not HasModelLoaded(GetHashKey("A_C_SharkTiger")) do
+		Wait(20)
+	end
 		
 	local pos = GetEntityCoords(PlayerPedId())
 	local ped = CreatePed(29, 0x06C3F072, pos.x, pos.y, pos.z, 90.0, true, false)
--- fishing/server/main.lua
+++ fishing/server/main.lua
@@ -1,102 +1,123 @@
-local ox_inventory = exports.ox_inventory -- ox_inventory for inventory handling
-
+local ox_inventory = exports.ox_inventory
 print("Fishing by FelixL has started :)")
 
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+local fishingPlayers = {}
+
+RegisterNetEvent('fishing:start')
+AddEventHandler('fishing:start', function()
+    local src = source
+    if not fishingPlayers[src] then
+        fishingPlayers[src] = true
+    end
+end)
+
+RegisterNetEvent('fishing:stop')
+AddEventHandler('fishing:stop', function()
+    local src = source
+    fishingPlayers[src] = nil
+end)
+
+
+AddEventHandler('playerDropped', function()
+    local src = source
+    fishingPlayers[src] = nil
+end)
+
 ESX.RegisterUsableItem('turtlebait', function(source)
-	local _source = source
-	xPlayer = ESX.GetPlayerFromId(_source)
-	if xPlayer.getInventoryItem('fishingrod').count > 0 then
-		TriggerClientEvent('fishing:setbait', _source, "turtle")
-		xPlayer.removeInventoryItem('turtlebait', 1)
-		TriggerClientEvent('fish:message', _source, "~g~You attached a turtle bait to your fishing rod!")
-	else
-		TriggerClientEvent('fish:message', _source, "~r~You don't have a fishing rod.")
-	end
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if xPlayer and xPlayer.getInventoryItem('fishingrod').count > 0 then
+        TriggerClientEvent('fishing:setbait', source, "turtle")
+        xPlayer.removeInventoryItem('turtlebait', 1)
+        TriggerClientEvent('fish:message', source, "~g~You attached turtle bait!")
+    else
+        TriggerClientEvent('fish:message', source, "~r~You don't have a fishing rod.")
+    end
 end)
 
 ESX.RegisterUsableItem('fishbait', function(source)
-	local _source = source
-	xPlayer = ESX.GetPlayerFromId(_source)
-	if xPlayer.getInventoryItem('fishingrod').count > 0 then
-		TriggerClientEvent('fishing:setbait', _source, "fish")
-		xPlayer.removeInventoryItem('fishbait', 1)
-		TriggerClientEvent('fish:message', _source, "~g~You attached a fish bait to your fishing rod!")
-	else
-		TriggerClientEvent('fish:message', _source, "~r~You don't have a fishing rod.")
-	end
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if xPlayer and xPlayer.getInventoryItem('fishingrod').count > 0 then
+        TriggerClientEvent('fishing:setbait', source, "fish")
+        xPlayer.removeInventoryItem('fishbait', 1)
+        TriggerClientEvent('fish:message', source, "~g~You attached fish bait!")
+    else
+        TriggerClientEvent('fish:message', source, "~r~You don't have a fishing rod.")
+    end
 end)
 
 ESX.RegisterUsableItem('turtle', function(source)
-	local _source = source
-	xPlayer = ESX.GetPlayerFromId(_source)
-	if xPlayer.getInventoryItem('fishingrod').count > 0 then
-		TriggerClientEvent('fishing:setbait', _source, "shark")
-		xPlayer.removeInventoryItem('turtle', 1)
-		TriggerClientEvent('fish:message', _source, "~g~You attached turtle meat to your fishing rod!")
-	else
-		TriggerClientEvent('fish:message', _source, "~r~You don't have a fishing rod.")
-	end
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if xPlayer and xPlayer.getInventoryItem('fishingrod').count > 0 then
+        TriggerClientEvent('fishing:setbait', source, "shark")
+        xPlayer.removeInventoryItem('turtle', 1)
+        TriggerClientEvent('fish:message', source, "~g~You attached turtle meat!")
+    else
+        TriggerClientEvent('fish:message', source, "~r~You don't have a fishing rod.")
+    end
 end)
 
 ESX.RegisterUsableItem('fishingrod', function(source)
-	local _source = source
-	TriggerClientEvent('fishing:fishstart', _source)
+    TriggerClientEvent('fishing:fishstart', source)
+    TriggerEvent('fishing:start', source)
 end)
 
 RegisterNetEvent('fishing:catch')
 AddEventHandler('fishing:catch', function(bait)
-    _source = source
-    local quantity = math.random(1, 3)
-    local rnd = math.random(1, 100)
-    xPlayer = ESX.GetPlayerFromId(_source)
+    local src = source
+    local xPlayer = ESX.GetPlayerFromId(src)
 
-    local canCarry = ox_inventory:CanCarryItem(xPlayer.source, 'fish', quantity)
-    if not canCarry then
-        TriggerClientEvent('esx:showNotification', _source, '~r~You don\'t have enough space in your inventory!')
+    -- Exploit protection
+    if not fishingPlayers[src] then
+        print(('[Exploit attempt] Player %s called fishing:catch without fishing:start'):format(src))
+        DropPlayer(src, "Fishing exploit detected. You have been kicked.")
         return
     end
 
+    fishingPlayers[src] = nil
+
+    if not xPlayer then return end
+
+    local quantity = math.random(1, 3)
+    local rnd = math.random(1, 100)
+
     if bait == "turtle" then
         if rnd <= 5 then
-            TriggerClientEvent('fishing:setbait', _source, "none")
-            TriggerClientEvent('esx:showNotification', _source, "~r~Your bait was damaged and fell off.")
-        elseif rnd > 5 and rnd < 98.5 then
-            TriggerClientEvent('fishing:message', _source, "~g~You caught " .. quantity .. " fish!")
-            xPlayer.addInventoryItem('fish', quantity)
-        elseif rnd >= 98.5 then
-            TriggerClientEvent('fishing:setbait', _source, "none")
-            TriggerClientEvent('fishing:message', _source, "~r~The fish was too big and broke your fishing rod.")
-            TriggerClientEvent('fishing:break', _source)
+            TriggerClientEvent('fishing:setbait', src, "none")
+            TriggerClientEvent('esx:showNotification', src, "~r~Your bait fell off.")
+        elseif rnd < 98.5 then
+            if ox_inventory:CanCarryItem(src, 'fish', quantity) then
+                xPlayer.addInventoryItem('fish', quantity)
+                TriggerClientEvent('fish:message', src, "~g~You caught " .. quantity .. " fish!")
+            else
+                TriggerClientEvent('esx:showNotification', src, "~r~Not enough inventory space!")
+            end
+        else
+            TriggerClientEvent('fishing:setbait', src, "none")
+            TriggerClientEvent('fish:message', src, "~r~The fish was too strong and broke your rod.")
+            TriggerClientEvent('fishing:break', src)
             xPlayer.removeInventoryItem('fishingrod', 1)
         end
+
     elseif bait == "fish" then
         if rnd <= 5 then
-            TriggerClientEvent('fishing:setbait', _source, "none")
-            TriggerClientEvent('esx:showNotification', _source, "~r~Your bait was damaged and fell off.")
+            TriggerClientEvent('fishing:setbait', src, "none")
+            TriggerClientEvent('esx:showNotification', src, "~r~Your bait fell off.")
         else
             local fishType = math.random(1, 5)
-            if fishType == 1 then
-                TriggerClientEvent('fishing:message', _source, "~g~You caught " .. quantity .. " anchovies!")
-                xPlayer.addInventoryItem('anchovy', quantity)
-            elseif fishType == 2 then
-                TriggerClientEvent('fishing:message', _source, "~g~You caught " .. quantity .. " trouts!")
-                xPlayer.addInventoryItem('trout', quantity)
-            elseif fishType == 3 then
-                TriggerClientEvent('fishing:message', _source, "~g~You caught " .. quantity .. " haddocks!")
-                xPlayer.addInventoryItem('haddock', quantity)
-            elseif fishType == 4 then
-                TriggerClientEvent('fishing:message', _source, "~g~You caught " .. quantity .. " salmons!")
-                xPlayer.addInventoryItem('salmon', quantity)
-            elseif fishType == 5 then
-                TriggerClientEvent('fishing:message', _source, "~g~You caught " .. quantity .. " groupers!")
-                xPlayer.addInventoryItem('grouper', quantity)
+            local item = ({'anchovy', 'trout', 'haddock', 'salmon', 'grouper'})[fishType]
+
+            if ox_inventory:CanCarryItem(src, item, quantity) then
+                xPlayer.addInventoryItem(item, quantity)
+                TriggerClientEvent('fish:message', src, ("~g~You caught %s %s!"):format(quantity, item))
+            else
+                TriggerClientEvent('esx:showNotification', src, "~r~Not enough inventory space!")
             end
         end
+
     elseif bait == "none" then
-        TriggerClientEvent('fishing:message', _source, "~y~You're currently fishing without bait.")
-        TriggerClientEvent('esx:showNotification', _source, "~y~You're currently fishing without bait!")
+        TriggerClientEvent('esx:showNotification', src, "~y~You are fishing without bait!")
     end
 end)
