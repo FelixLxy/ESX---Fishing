91cdce275b10afafb4f829bc7bb45e62130755c2
FelixLxy <felixlackner27@gmail.com>
2025-06-14 11:01:02 +0200

    Fishing

--- fishing/server/main.lua
+++ fishing/server/main.lua
@@ -104,26 +104,29 @@ AddEventHandler('fishing:catch', function(bait)
 
     local quantity = math.random(1, 3)
     local rnd = math.random(1, 100)
+    local fishTypes = { 'anchovy', 'trout', 'haddock', 'salmon', 'grouper' }
 
-    if bait == "turtle" then
-        if rnd <= 5 then
-            TriggerClientEvent('fishing:setbait', src, "none")
-            TriggerClientEvent('esx:showNotification', src, "~r~Your bait fell off.")
-        elseif rnd < 98.5 then
-            if ox_inventory:CanCarryItem(src, 'fish', quantity) then
-                xPlayer.addInventoryItem('fish', quantity)
-                TriggerClientEvent('fish:message', src, "~g~You caught " .. quantity .. " fish!")
-            else
-                TriggerClientEvent('esx:showNotification', src, "~r~Not enough inventory space!")
-            end
+if bait == "turtle" then
+    if rnd <= 5 then
+        TriggerClientEvent('fishing:setbait', src, "none")
+        TriggerClientEvent('esx:showNotification', src, "~r~Your bait fell off.")
+    elseif rnd < 98.5 then
+        local caughtFish = fishTypes[math.random(#fishTypes)]
+        if ox_inventory:CanCarryItem(src, caughtFish, quantity) then
+            xPlayer.addInventoryItem(caughtFish, quantity)
+            TriggerClientEvent('fish:message', src, "~g~You caught " .. quantity .. " " .. caughtFish .. "!")
         else
-            TriggerClientEvent('fishing:setbait', src, "none")
-            TriggerClientEvent('fish:message', src, "~r~The fish was too strong and broke your rod.")
-            TriggerClientEvent('fishing:break', src)
-            xPlayer.removeInventoryItem('fishingrod', 1)
-            -- Angeln stoppen
-            TriggerEvent('fishing:stop', src)
+            TriggerClientEvent('esx:showNotification', src, "~r~Not enough inventory space!")
         end
+    else
+        TriggerClientEvent('fishing:setbait', src, "none")
+        TriggerClientEvent('fish:message', src, "~r~The fish was too strong and broke your rod.")
+        TriggerClientEvent('fishing:break', src)
+        xPlayer.removeInventoryItem('fishingrod', 1)
+        TriggerEvent('fishing:stop', src)
+    end
+end
+
 
     elseif bait == "fish" then
         if rnd <= 5 then
